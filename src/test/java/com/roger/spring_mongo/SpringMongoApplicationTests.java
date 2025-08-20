40a238a2aecdc1722df342d60f7b3016af61e47f
Roger Vasconcelos <vrogerio87@gmail.com>
2025-08-20 15:06:11 -0300

    Projeto criado

---
+++ src/test/java/com/roger/spring_mongo/SpringMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roger.spring_mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
