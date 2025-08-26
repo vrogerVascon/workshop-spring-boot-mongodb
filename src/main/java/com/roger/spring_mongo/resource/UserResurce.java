0812b8bd6dc20913c37299d2b86840770fe3afeb
Roger Vasconcelos <vrogerio87@gmail.com>
2025-08-26 11:49:37 -0300

    Atualizacao de usuario com PUT

--- src/main/java/com/roger/spring_mongo/resource/UserResurce.java
+++ src/main/java/com/roger/spring_mongo/resource/UserResurce.java
@@ -60,6 +60,14 @@ public class UserResurce {
         return ResponseEntity.noContent().build();
     }
 
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
+        User obj = service.fromUserDTO(objDto);
+        obj.setId(id);
+        obj = service.update(obj);
+        return ResponseEntity.noContent().build();
+    }
+
 
 
 }
