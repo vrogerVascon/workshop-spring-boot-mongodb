4a2de4cd949db6dd398379a78a3ea626302e8896
Roger Vasconcelos <vrogerio87@gmail.com>
2025-08-26 10:45:53 -0300

    Insercao de usuario com POST

--- src/main/java/com/roger/spring_mongo/resource/UserResurce.java
+++ src/main/java/com/roger/spring_mongo/resource/UserResurce.java
@@ -1,13 +1,16 @@
 package com.roger.spring_mongo.resource;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.roger.spring_mongo.domain.User;
 import com.roger.spring_mongo.dto.UserDTO;
 import com.roger.spring_mongo.services.UserService;
@@ -27,19 +30,28 @@ public class UserResurce {
 
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<List<UserDTO>> findAll() {
-
         List<User> list = service.findAll();
         List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
         return ResponseEntity.ok().body(listDTO);
     }
 
+
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public ResponseEntity<UserDTO> findById(@PathVariable String id) {
         User obj = service.findById(id);
-
-
-
         return ResponseEntity.ok().body(new UserDTO(obj));
+    }
+
+
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+        User obj = service.fromUserDTO(objDto);
+        obj = service.insert(obj);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 
 
--- src/main/java/com/roger/spring_mongo/services/UserService.java
+++ src/main/java/com/roger/spring_mongo/services/UserService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.roger.spring_mongo.domain.User;
+import com.roger.spring_mongo.dto.UserDTO;
 import com.roger.spring_mongo.repository.UserRepository;
 import com.roger.spring_mongo.services.exception.ObjectNotFoundException;
 
@@ -23,6 +24,14 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    public User fromUserDTO(UserDTO objDTO) {
+        return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
     }
 
 }
