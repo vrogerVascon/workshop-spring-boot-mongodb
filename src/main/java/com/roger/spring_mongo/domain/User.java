10522646245389d226abc4eaa8c36ec748ab54ff
Roger Vasconcelos <vrogerio87@gmail.com>
2025-08-27 15:42:54 -0300

    Referenciando os posts do usuario

--- src/main/java/com/roger/spring_mongo/domain/User.java
+++ src/main/java/com/roger/spring_mongo/domain/User.java
@@ -1,7 +1,10 @@
 package com.roger.spring_mongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "users")
@@ -11,6 +14,9 @@ public class User implements Serializable {
     private String id;
     private String name;
     private String email;
+
+    @DBRef(lazy = true)
+    private List<Post> posts = new ArrayList<>();
 
     public User() {
 
@@ -45,6 +51,15 @@ public class User implements Serializable {
     public void setEmail(String email) {
         this.email = email;
     }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
+
 
     /* */
     @Override
